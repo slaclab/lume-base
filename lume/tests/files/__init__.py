601822c6428c6f70c3d50a00ad85df0b6c33362d
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-07-05 15:11:21 -0700

    Check in test files

---
+++ lume/tests/files/__init__.py
@@ -0,0 +1,3 @@
+from pkg_resources import resource_filename
+
+LUME_CONFIG_YAML = resource_filename("lume.tests.files", "test_lume_config.yml")
---
+++ lume/tests/files/test_command_wrapper_subclass.py
@@ -0,0 +1,62 @@
+from random import random
+from lume.base import CommandWrapper, tools
+import os
+import yaml
+import numpy as np
+
+class MyModelClass(CommandWrapper):
+    def __init__(self, *args, variables=None, input_image=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._input_image = input_image
+        self._variables = variables
+
+
+    #implementation of abstract method
+    @staticmethod
+    def input_parser(path):
+        config = {}
+
+        if os.path.exists(tools.full_path(path)):
+            yaml_file = tools.full_path(path)
+            config = yaml.safe_load(open(yaml_file))
+
+
+            # convention for this is that any input file paths are relative the input yaml directory
+            if "input_image" in config:
+                
+                # Get the yaml file root
+                root, _ = os.path.split(tools.full_path(path))
+                input_image_path = os.path.join(root, config["input_image"])
+
+                if os.path.exists(tools.full_path(input_image_path)):
+                    with open(input_image_path, "rb") as f:
+                        config["input_image"] = np.load(f)
+
+                else:
+                    print(f"unable to resolve input impage path {input_image_path}")
+
+        else:
+            print(f"unable to parse model input file at {path}")
+                
+        return config
+
+    def archive(self):
+        ...
+
+    def configure(self):
+        ...
+
+    def load_archive(self):
+        ...
+
+    def load_output(self):
+        ...
+
+    def plot(self):
+        ...
+
+    def run(self):
+        ...
+
+    def write_input(self):
+        ...
