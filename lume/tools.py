900343217faf32c99e75324048e657d6dab1c8ad
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2022-07-05 14:29:02 -0700

    Add make_executable, find_executable

--- lume/tools.py
+++ lume/tools.py
@@ -2,6 +2,7 @@ import hashlib
 import json
 import numpy as np
 import os
+import shutil
 
 
 class NpEncoder(json.JSONEncoder):
@@ -63,3 +64,49 @@ def full_path(path):
         The expanded absolute path
     """
     return os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
+
+
+def make_executable(path):
+    """
+    Makes a file executable.
+
+    https://stackoverflow.com/questions/12791997/how-do-you-do-a-simple-chmod-x-from-within-python
+    """
+    mode = os.stat(path).st_mode
+    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    os.chmod(path, mode)
+
+
+def find_executable(exename=None, envname=None):
+    """
+    Finds an executable from a given name or environmental variable.
+
+    If neither are files, the path will be searched for exename
+
+    """
+
+    # Simply return if this exists
+    if exename and os.path.isfile(exename):
+        assert os.access(exename, os.X_OK), f'File is not executable: {exename}'
+        return full_path(exename)
+
+    envexe = os.environ.get(envname)
+    if envexe and os.path.isfile(envexe):
+        assert os.access(envexe, os.X_OK), f'File is not executable: {envexe}'
+        return full_path(envexe)
+
+    if not exename and not envname:
+         raise ValueError('No exename or envname ')
+
+    # Start searching
+    search_path = []
+    #search_path.append(os.environ.get(envname))
+    search_path.append(os.getcwd())
+    search_path.append(os.environ.get('PATH'))
+    search_path_str = os.pathsep.join(search_path)
+    bin_location = shutil.which(exename, path=search_path_str)
+
+    if bin_location and os.path.isfile(bin_location):
+        return full_path(bin_location)
+
+    raise ValueError(f'Could not find executable: exename={exename}, envname={envname}')
