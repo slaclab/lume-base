d976a3b6a222007e2f1f23e908a12f5174f3ffef
Ryan Roussel <rroussel@slac.stanford.edu>
2026-01-28 11:22:39 -0600

    update pkg_resources to importlib

--- lume/tests/files/__init__.py
+++ lume/tests/files/__init__.py
@@ -1,4 +1,4 @@
-from pkg_resources import resource_filename
+from importlib import resources
 
-LUME_CONFIG_YAML = resource_filename("lume.tests.files", "test_lume_config.yml")
-INPUT_YAML = resource_filename("lume.tests.files", "test_input_file.yml")
+LUME_CONFIG_YAML = str(resources.files("lume.tests.files") / "test_lume_config.yml")
+INPUT_YAML = str(resources.files("lume.tests.files") / "test_input_file.yml")
