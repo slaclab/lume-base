8e979da951660463dfe6ef7c9d8f874cc03cc72f
Ryan Roussel <rroussel@slac.stanford.edu>
2026-02-12 11:33:32 -0600

    move variables to dedicated sub-package

---
+++ lume/variables/__init__.py
@@ -0,0 +1,4 @@
+from lume.variables.scalar import ScalarVariable
+from lume.variables.variable import Variable
+
+__all__ = ["Variable", "ScalarVariable"]
--- lume/variables/scalar.py
+++ lume/variables/scalar.py
@@ -1,49 +1,8 @@
-"""
-This module contains definitions of LUME-model variables for use with lume tools.
-Variables are designed as pure descriptors and thus aren't intended to hold actual values,
-but they can be used to validate encountered values.
-
-"""
-
-from abc import ABC, abstractmethod
-from typing import Any
-from enum import Enum
 import warnings
-
 import numpy as np
-from pydantic import BaseModel, field_validator, model_validator
+from pydantic import field_validator, model_validator
 
-
-class ConfigEnum(str, Enum):
-    """Enum for configuration options during validation."""
-
-    NULL = "none"
-    WARN = "warn"
-    ERROR = "error"
-
-
-class Variable(BaseModel, ABC):
-    """Abstract variable base class.
-
-    Attributes
-    -----------
-    name: str
-        Name of the variable.
-    read_only: bool
-        Flag indicating whether the variable can be set.
-    """
-
-    name: str
-    read_only: bool = False
-    default_validation_config: ConfigEnum = "none"
-
-    @abstractmethod
-    def validate_value(self, value: Any, config: ConfigEnum = None):
-        pass
-
-    def model_dump(self, **kwargs) -> dict[str, Any]:
-        config = super().model_dump(**kwargs)
-        return {"variable_class": self.__class__.__name__} | config
+from lume.variables.variable import Variable, ConfigEnum
 
 
 class ScalarVariable(Variable):
