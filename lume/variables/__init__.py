faddeedc703d979177ccd94c4e9a61213df8903f
Ryan Roussel <rroussel@slac.stanford.edu>
2026-02-12 11:46:18 -0600

    add particle variable group to init

--- lume/variables/__init__.py
+++ lume/variables/__init__.py
@@ -1,4 +1,5 @@
 from lume.variables.scalar import ScalarVariable
 from lume.variables.variable import Variable
+from lume.variables.particle_group import ParticleGroupVariable
 
-__all__ = ["Variable", "ScalarVariable"]
+__all__ = ["Variable", "ScalarVariable", "ParticleGroupVariable"]
