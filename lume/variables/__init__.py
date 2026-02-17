e12cfe4171efe6cd8950fab985d4fcdc87d64cab
Sara Miskovich <saramiskovich@gmail.com>
2026-02-17 13:04:16 -0800

    rebase and resolve merge conflict

--- lume/variables/__init__.py
+++ lume/variables/__init__.py
@@ -1,5 +1,12 @@
 from lume.variables.scalar import ScalarVariable
 from lume.variables.variable import Variable, ConfigEnum
 from lume.variables.particle_group import ParticleGroupVariable
+from lume.variables.ndvariable import NDVariable
 
-__all__ = ["Variable", "ConfigEnum", "ScalarVariable", "ParticleGroupVariable"]
+__all__ = [
+    "Variable",
+    "ConfigEnum",
+    "ScalarVariable",
+    "ParticleGroupVariable",
+    "NDVariable",
+]
